2f8f344e4ccb53980023c244df0ed44cc5cd4cf0
Deepesh Kumar Nath <deepesh@ost.com>
2018-08-12 10:59:11 +0530

    Added ERC20Mock

--- contracts/ERC20Interface.sol
+++ contracts/ERC20Interface.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.4.11;
 // Abstract contract for the full ERC 20 Token standard
 // https://github.com/ethereum/EIPs/issues/20
 
-contract ERC20Token {
+contract ERC20Interface {
     /* This is a slight change to the ERC20 base standard.
     function totalSupply() constant returns (uint256 supply);
     is replaced with:
---
+++ contracts/ERC20Mock.sol
@@ -0,0 +1,62 @@
+import "./ERC20Interface.sol";
+import "./SafeMath.sol";
+
+contract ERC20Mock is ERC20Interface{
+
+    using SafeMath for uint256;
+
+    mapping(address => uint256) balances;
+    mapping(address => mapping (address => uint256)) allowed;
+
+
+    constructor( uint256 _totalSupply) public {
+        totalSupply = _totalSupply;
+        balances[msg.sender]  = _totalSupply;
+        // According to the ERC20 standard, a token contract which creates new tokens should trigger
+        // a Transfer event and transfers of 0 values must also fire the event.
+        emit Transfer(0x0, msg.sender, _totalSupply);
+    }
+
+    function balanceOf(address _owner) public view returns (uint256) {
+        return balances[_owner];
+    }
+
+
+    function allowance(address _owner, address _spender) public view returns (uint256 remaining) {
+        return allowed[_owner][_spender];
+    }
+
+
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        // According to the EIP20 spec, "transfers of 0 values MUST be treated as normal
+        // transfers and fire the Transfer event".
+        // Also, should throw if not enough balance. This is taken care of by SafeMath.
+        balances[msg.sender] = balances[msg.sender].sub(_value);
+        balances[_to] = balances[_to].add(_value);
+
+        emit Transfer(msg.sender, _to, _value);
+
+        return true;
+    }
+
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        balances[_from] = balances[_from].sub(_value);
+        allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
+        balances[_to] = balances[_to].add(_value);
+
+        emit Transfer(_from, _to, _value);
+
+        return true;
+    }
+
+
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+
+        allowed[msg.sender][_spender] = _value;
+
+        emit Approval(msg.sender, _spender, _value);
+
+        return true;
+    }
+}
